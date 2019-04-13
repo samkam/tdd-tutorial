8360c0239edf0df66aabf8d97f5d547b0246a941
Sam Kamenetz <samkamenetz@gmail.com>
2019-04-12 21:36:48 -0400

    added checking for second list item to functional test

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+	
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# A first time user checks out to-do list append.
 		# They navigate to the home page
@@ -34,16 +39,19 @@ class NewVisitorTest(unittest.TestCase):
 		
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			self.assertIn('1: Buy peacock feathers', [row.text for row in rows]),
-			f"New to-do item did not appear in table. contents were:\n{table.text}"
-		)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		self.fail("finish this test!")
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and now shows both items on her list
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
+		self.fail("finish this test!")
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.
