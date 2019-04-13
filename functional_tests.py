66af57eabdbba196a62ca755f4942593037ad6cd
Sam Kamenetz <samkamenetz@gmail.com>
2019-04-12 22:45:14 -0400

    refactored NewvisitorTest to use helper function for checking table list

--- functional_tests.py
+++ functional_tests.py
@@ -11,7 +11,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def check_for_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_tag_name('tr')
+		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
 	
 	def test_can_start_a_list_and_retrieve_it_later(self):
@@ -39,17 +39,17 @@ class NewVisitorTest(unittest.TestCase):
 		
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_in_list_table('1: Buy peacock feathers')
 		
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-
+		time.sleep(1)
 		# The page updates again, and now shows both items on her list
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_in_list_table('1: Buy peacock feathers')
+		self.check_for_in_list_table('2: Use peacock feathers to make a fly')
 
 		self.fail("finish this test!")
 		# Edith wonders whether the site will remember her list. Then she sees
