ba548159e885897a1d3b6a5804b9874d8a4d9687
Sam Kamenetz <samkamenetz@gmail.com>
2019-04-21 20:18:33 -0400

    added bootstrap for styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,15 +1,31 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 MAX_WAIT = 4
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 	def tearDown(self):
 		self.browser.quit()
+
+	def test_layout_and_styling(self):
+		#Edith goes to the homepage
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+		# she notices the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] +inputbox.size['width']/2, 512, delta=10)
+		
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table("1: testing")
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] +inputbox.size['width']/2, 512, delta=10)
 
 	def wait_for_row_in_list_table(self, row_text):
 		start_time = time.time()
