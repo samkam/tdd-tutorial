74a850b777f649bae82fa06ba3759a30882c4f8c
Sam Kamenetz <samkamenetz@gmail.com>
2019-04-27 18:06:49 -0400

    temporary fix for issue RE key

--- superlists/settings.py
+++ superlists/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-with open('etc\secret_key.txt') as f:
-	SECRET_KEY = f.read().strip()
+#with open('etc\secret_key.txt') as f:
+SECRET_KEY = 'dev'#f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
