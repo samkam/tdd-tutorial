1547f867d9ac19b258a2b337416ed969522677a3
Sam Kamenetz <samkamenetz@gmail.com>
2019-05-06 22:57:54 -0400

    changed ALLOWED_HOSTS for initial deploy (fixed error)

--- superlists/settings.py
+++ superlists/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'dev'#f.read().strip()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
