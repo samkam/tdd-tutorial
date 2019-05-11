4b39ba43146dc7197ec15110822a220e250e43d9
Sam Kamenetz <samkamenetz@gmail.com>
2019-05-11 17:55:01 -0400

    use new form in home_page, simplify tests. functional_tests failing

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from lists.views import home_page
 from lists.models import Item, List
 from django.utils.html import escape
+from lists.forms import ItemForm
 class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
@@ -11,6 +12,10 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		response = self.client.get('')
 		self.assertTemplateUsed(response, 'home.html')
+	
+	def test_home_page_uses_item_form(self):
+		response = self.client.get('')
+		self.assertIsInstance(response.context['form'],ItemForm)
 
 
 class ListViewTest(TestCase):
